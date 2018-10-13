3512e732db6095df8eeeebb778a40adc425359b7
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2018-10-13 16:11:04 +0200

    Minimal app with unit test

---
+++ src/test/java/demo/AppTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+
+import org.junit.jupiter.api.Test;
+
+
+public class AppTest
+{
+    @Test
+    public void should_have_main() {
+        App.main(new String[]{"a string"});
+    }
+}
