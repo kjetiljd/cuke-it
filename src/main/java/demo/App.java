9d1a14a9e3a1c7216a0c6c065367eaa3fc95bc66
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2018-10-14 00:05:30 +0200

    Introduce web app

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,8 +1,31 @@
 package demo;
 
-public class App
-{
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+import ratpack.server.RatpackServer;
+import ratpack.server.ServerConfig;
+
+public class App {
+
+    private RatpackServer server;
+
+    public static void main(String[] args ) throws Exception {
+        new App().start();
+    }
+
+    void start() throws Exception {
+        server = RatpackServer.start(server -> server
+                .serverConfig(ServerConfig.embedded())
+                .registryOf(registry -> registry.add("World!"))
+                .handlers(chain -> chain
+                        .get(ctx -> ctx.render("Hello " + ctx.get(String.class))))
+                );
+
+    }
+
+    String port() {
+        return "" + server.getBindPort();
+    }
+
+    void stop() throws Exception {
+        server.stop();
     }
 }
--- src/test/java/demo/AppTest.java
+++ src/test/java/demo/AppTest.java
@@ -1,13 +1,38 @@
 package demo;
 
 
+import org.apache.http.client.fluent.Request;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AppTest
-{
-    @Test
-    public void should_have_main() {
-        App.main(new String[]{"a string"});
+
+public class AppTest {
+
+    private static App app;
+
+    @BeforeAll
+    static void startApp() throws Exception {
+        app = new App();
+        app.start();
     }
+
+    @AfterAll
+    static void stopApp() throws Exception {
+        app.stop();
+    }
+
+    @Test
+    public void get_should_respond_with_hello_world() throws Exception {
+        // given
+
+        // when
+        String reponse = Request.Get("http://localhost:" + app.port() + "/").execute().returnContent().asString();
+
+        // then
+        assertEquals("Hello World!", reponse);
+    }
+
 }
